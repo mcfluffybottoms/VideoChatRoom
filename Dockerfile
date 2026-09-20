0cc73db2d43922b209ac087600e3672d8603dcd7
Алтунина Екатерина <166242132+mcfluffybottoms@users.noreply.github.com>
2026-09-20 07:14:33 +0300

    feat: update Dockerfile and configuration for improved build process, add .dockerignore

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,33 @@
-# REACT
+# CLIENT BUILD
 FROM node:22-alpine AS client-build
 
 WORKDIR /app/client
 
 COPY client/package*.json ./
-RUN npm install
+RUN npm ci
 
 COPY client/ ./
+
 RUN npm run build
 
-# NODE JS SERVER
+# SERVER
 FROM node:22-alpine
 
+WORKDIR /app/server
+
+COPY server/package*.json ./
+
+RUN npm ci
+
+COPY server/ ./
+
+# APP
 WORKDIR /app
 
-COPY server/package*.json ./server/
-RUN cd server && npm install --omit=dev
-
-COPY server/ ./server/
 COPY --from=client-build /app/client/dist ./client/dist
 
 ENV NODE_ENV=production
 
 EXPOSE 3000
 
-CMD ["node", "server/src/index.js"]
+CMD ["npm", "start", "--prefix", "server"]
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,44 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-        https: {
-            key: fs.readFileSync('../certs/key.pem'),
-            cert: fs.readFileSync('../certs/cert.pem'),
-        },
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+    };
+
+    if (command === 'serve') {
+        const keyPath = path.resolve(__dirname, 'certs/key.pem');
+        const certPath = path.resolve(__dirname, 'certs/cert.pem');
+
+        config.server = {
+            host: '0.0.0.0',
+
+            https: {
+                key: fs.readFileSync(keyPath),
+                cert: fs.readFileSync(certPath),
             },
-            '/socket.io': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                ws: true,
+
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                },
+
+                '/socket.io': {
+                    target: 'http://localhost:3000',
+                    changeOrigin: true,
+                    ws: true,
+                },
             },
-        },
-    },
+        };
+    }
+
+    return config;
 });
